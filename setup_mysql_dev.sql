7cee8b752af13c615a394ad8881154c17a9c3d67
David <1508@holbertonschool.com>
2020-08-11 15:12:10 +0000

    Add sql to setup a server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Script that prepares a MySQL server for the project
+-- Seting up a database and user privileges
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
