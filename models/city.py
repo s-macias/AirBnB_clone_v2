b5f2ca3ad7f5da7f591981df65865764924fd03e
s-macias <1552@holbertonschool.com>
2020-08-12 19:29:54 +0000

    Task 6 city.py

--- models/city.py
+++ models/city.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.user import User
-from models.amenity import Amenity
 from models.place import Place
-from models.review import Review
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
