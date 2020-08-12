f069097043e0123e87b93e1b16ef434d28ff4518
s-macias <1552@holbertonschool.com>
2020-08-12 20:12:42 +0000

    Task 7 fixing modules imported

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
+class User(BaseModel, Base):
+    """This class defines  User class """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
