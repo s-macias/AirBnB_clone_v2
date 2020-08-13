9b4bfc2eafc9be840d6d33a1ef4bd4e0a4d3c1a4
David <1508@holbertonschool.com>
2020-08-13 14:50:19 +0000

    Fix relationship

--- models/review.py
+++ models/review.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = "reviews"
-    place_id = Column(String(60), nullable=False)
-    user_id = Column(String(60), nullable=False)
     text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    
