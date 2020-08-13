2f4ebe52400b2a669b23b673024d337f70ca28e6
s-macias <1552@holbertonschool.com>
2020-08-13 18:15:08 +0000

    Task 10

--- models/place.py
+++ models/place.py
@@ -2,10 +2,19 @@
 """ Place Module for HBNB project """
 import models
 from os import getenv
-from models.base_model import BaseModel, Base
 from models.review import Review
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
+
+
+metadata = Base.metadata
+place_amenity = Table('place_amenity', metadata,
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey(
+                          'amenities.id'), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -23,6 +32,8 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
     reviews = relationship('Review', cascade='all, delete', backref='place')
+    amenities = relationship(
+        'Amenity', secondary=place_amenity, viewonly=False)
 
     @property
     def reviews(self):
@@ -33,3 +44,19 @@ class Place(BaseModel, Base):
             if review.place_id == self.id:
                 list_review.append(review)
         return list_review
+
+    @property
+    def amenities(self):
+        """Getter for Amenity instances"""
+        list_amenities = []
+        dict_amenities = models.storage.all('Amenity')
+        for amenity in dict_amenities.values():
+            if amenity.id in self.amenity_ids:
+                list_amenities.append(amenity)
+        return list_amenities
+
+    @amenities.setter
+    def amenities(self, obj):
+        """Setter for Amenity instances"""
+        if type(obj) == Amenity:
+            self.amenity_ids.append(obj.id)
