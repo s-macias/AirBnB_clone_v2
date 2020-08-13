e5469d9f4f1e07215edf5803c3b22868b2b4dc07
David <1508@holbertonschool.com>
2020-08-13 22:02:10 +0000

    Remove duplicate storage

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+
         if "created_at" in kwargs:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
