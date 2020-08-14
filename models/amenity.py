6b49b388d0414e2d8c376b8b3a1b6cbf52c0f992
s-macias <1552@holbertonschool.com>
2020-08-14 00:09:50 +0000

    add clean up

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Amenity(BaseModel, Base):
